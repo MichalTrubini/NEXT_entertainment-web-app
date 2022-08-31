42913bf48696ecca6a2447846fe19c98735499e2
Michal Trubini <mchl.trubini@gmail.com>
2022-08-31 15:04:48 +0200

    boiler-plate

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;500&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
