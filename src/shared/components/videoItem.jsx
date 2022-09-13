54870d687b0c762457e5285312202033a06ce728
Michal Trubini <mchl.trubini@gmail.com>
2022-09-13 13:16:19 +0200

    active states

--- src/shared/components/videoItem.jsx
+++ src/shared/components/videoItem.jsx
@@ -1,15 +1,29 @@
 import Image from "next/image";
 import styles from "./videoItem.module.css";
 import bookmark from "../../../public/assets/icon-bookmark-empty.svg";
+import playIcon from "../../../public/assets/icon-play.svg";
+import { useState } from "react";
 
 const VideoItem = (props) => {
+  const [isShown, setIsShown] = useState(false);
+
   return (
     <div className={styles.video}>
       <div className={styles.bookmarkContainer}>
-        <Image src={bookmark} />
+        <Image src={bookmark} className={styles.bookmarkHover}/>
       </div>
-      <div className={`${styles.imageContainer} ${props.classNameImage}`}>
-        <Image src={props.src} alt={props.alt} layout='fill'/>
+      <div
+        className={`${styles.imageContainer} ${props.classNameImage}`}
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+      >
+        <Image src={props.src} alt={props.alt} layout="fill" />
+        <div className={isShown ? `${styles.hoverOverlay} ${styles.hoverOn}` : `${styles.hoverOverlay} ${styles.hoverOff}`}>
+          <div className={styles.hoverPlay}>
+            <Image src={playIcon} />
+            <p className={styles.play}>Play</p>
+          </div>
+        </div>
       </div>
       <div className={props.classNameRow}>
         <div className={`${styles.list} ${props.classNameTopRow}`}>
@@ -22,7 +36,9 @@ const VideoItem = (props) => {
           </div>
           <p className={`${styles.rating} ${styles.disc}`}>{props.rating}</p>
         </div>
-        <h2 className={`${styles.title} ${props.classNameBottomRow}`}>{props.title}</h2>
+        <h2 className={`${styles.title} ${props.classNameBottomRow}`}>
+          {props.title}
+        </h2>
       </div>
     </div>
   );
