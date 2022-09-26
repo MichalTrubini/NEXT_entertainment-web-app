1a82d0fcec0dc5f9b0375b9d18be18e385dd1abc
Michal Trubini <90413120+MichalTrubini@users.noreply.github.com>
2022-09-26 17:18:07 +0200

    Delete src/lib directory

--- src/lib/db.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from "mongodb";
-
-export default async function connectToDatabase() {
-  const client = await MongoClient.connect(
-    "mongodb+srv://frontendMentor:frontendMentor@cluster0.gociwcj.mongodb.net/entertainment?retryWrites=true&w=majority"
-  );
-
-  return client;
-}
